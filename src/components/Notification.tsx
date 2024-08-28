ad3691786da89d1e5d194f087f67adef85470fb8
gebrina <gsonofmary@gmail.com>
2024-08-28 20:35:16 +0300

    add icons to Notification component

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,4 +1,8 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import CloseIcon from "../assets/close-icon.svg";
+import ErroIcon from "../assets/error-icon.svg";
+import SuccessIcon from "../assets/success-icon.svg";
+import WarningIcon from "../assets/warning-icon.svg";
 
 type TNotificationType = "error" | "warning" | "success";
 
@@ -7,10 +11,26 @@ type TNotificationProps = {
   type?: TNotificationType;
 };
 
-export const Notification: FC<TNotificationProps> = ({ message }) => {
+export const Notification: FC<TNotificationProps> = ({
+  message,
+  type = "success",
+}) => {
+  const [show, setShow] = useState(true);
+
+  const NotificationIcon =
+    type === "error"
+      ? ErroIcon
+      : type === "warning"
+      ? WarningIcon
+      : SuccessIcon;
+
   return (
-    <div className="h-12 text-xl animate-ping py-2 px-3 rounded-xl fixed bottom-20 right-10 w-1/2 bg-red-700 text-white">
-      <p>{message}</p>
+    <div className="h-12 text-xl flex justify-between items-center py-2 px-3 rounded-xl fixed bottom-20 right-10 w-1/2 bg-red-700 text-white">
+      <div className="flex items-center gap-2">
+        <img className="h-6" src={NotificationIcon} />
+        <p>{message}</p>
+      </div>
+      <img className="h-6" src={CloseIcon} />
     </div>
   );
 };
