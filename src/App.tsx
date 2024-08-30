32f0f383266acb3838a483fe11c2b6ffba94d8af
gebrina <gsonofmary@gmail.com>
2024-08-30 16:24:09 +0300

    render routes based on location

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Footer, Navbar } from "./components";
 import { ConfirmEmail } from "./pages/ConfirmEmail";
 import { CreateAccount } from "./pages/CreateAccount";
-import { DashboardRoutes } from "./pages/dashboard/DashboardRoutes";
 import { ForgotPassword } from "./pages/ForgotPassword";
 import { Login } from "./pages/Login";
 import { NotFound } from "./pages/NotFound";
 import { Products } from "./pages/Products";
 import { ResetPassword } from "./pages/ResetPassword";
+import { DashboardRoutes } from "./pages/dashboard/DashboardRoutes";
 
 export const App = () => {
   const [showNavandFooter, setShowNavandFooter] = useState(true);
@@ -18,41 +18,35 @@ export const App = () => {
   }, []);
 
   return (
-    <div
-      className={`${
-        showNavandFooter && "sm:container"
-      } overflow-hidden flex  flex-col gap-8 mx-auto  h-full w-full`}
-    >
-      <BrowserRouter>
-        {showNavandFooter && (
-          <>
+    <BrowserRouter>
+      {showNavandFooter ? (
+        <div
+          className="sm:container
+                    overflow-hidden flex 
+                    flex-col gap-8 mx-auto  h-full w-full"
+        >
+          <div className="h-12">
+            <Navbar />
+          </div>
+          <div className="page-content mt-10 flex-1 overflow-y-auto">
+            <Routes>
+              <Route path="" element={<Products />} />
+              <Route path="login" element={<Login />} />
+              <Route path="create-account" element={<CreateAccount />} />
+              <Route path="confirm-email" element={<ConfirmEmail />} />
+              <Route path="forgot-password" element={<ForgotPassword />} />
+              <Route path="reset-password/:email" element={<ResetPassword />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
             <div className="h-12">
-              <Navbar />
+              <Footer />
             </div>
-            <div className="page-content mt-10 flex-1 overflow-y-auto">
-              <Routes>
-                <Route path="" element={<Products />} />
-                <Route path="login" element={<Login />} />
-                <Route path="create-account" element={<CreateAccount />} />
-                <Route path="confirm-email" element={<ConfirmEmail />} />
-                <Route path="forgot-password" element={<ForgotPassword />} />
-                <Route
-                  path="reset-password/:email"
-                  element={<ResetPassword />}
-                />
-                {showNavandFooter && <Route path="*" element={<NotFound />} />}
-              </Routes>
-            </div>
-          </>
-        )}
-
-        <DashboardRoutes />
-      </BrowserRouter>
-      {showNavandFooter && (
-        <div className="h-12">
-          <Footer />
+          </div>
         </div>
+      ) : (
+        <DashboardRoutes />
       )}
-    </div>
+      ;
+    </BrowserRouter>
   );
 };
